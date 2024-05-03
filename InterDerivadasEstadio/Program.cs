aa4785d2572f7980cdedb8c3098afe6a1a32aa7c
alexa <alexa@LXNDR1305>
2024-05-03 11:21:59 -0600

    Agregar archivos de proyecto.

---
+++ InterDerivadasEstadio/Program.cs
@@ -0,0 +1,29 @@
+﻿class Estadio
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("                   *   *   *   *   *   *   *   *   *   *   *   *   *");
+        Console.WriteLine("               *  *                                             *      *");
+        Console.WriteLine("            *      *                                           *          *");
+        Console.WriteLine("         *          *                    2                    *              *");
+        Console.WriteLine("       *             *                                       *                 *");
+        Console.WriteLine("     *                *                                     *                    *");
+        Console.WriteLine("   *                   *   *   *   *   *   *   *   *   *   *                       *");
+        Console.WriteLine("  *                                                                                 *");
+        Console.WriteLine(" *                     *                                   *                         *");
+        Console.WriteLine("*                                                                                     *");
+        Console.WriteLine("*                      *                                   *                          *");
+        Console.WriteLine("*        1                                                                3           *");
+        Console.WriteLine("*                      *                                   *                          *");
+        Console.WriteLine("*                                                                                     *");
+        Console.WriteLine(" *                     *                                   *                         *");
+        Console.WriteLine("  *                                                                                 *");
+        Console.WriteLine("   *                   *   *   *   *   *   *   *   *   *   *                       *");
+        Console.WriteLine("     *                *                                     *                    *");
+        Console.WriteLine("       *             *                                       *                 *");
+        Console.WriteLine("         *          *                    4                    *              *");
+        Console.WriteLine("            *      *                                           *          *");
+        Console.WriteLine("               *  *                                             *      *");
+        Console.WriteLine("                   *   *   *   *   *   *   *   *   *   *   *   *   *");
+    }
+}
