7e4c8f17a268bcb829a92fcf3152b85f3fd82f8e
Alexander <alexanderarevalo707@gmail.com>
2024-05-05 15:00:45 -0600

    CAMBIOS ESTADIO Y COLOR

--- InterDerivadasEstadio/Program.cs
+++ InterDerivadasEstadio/Program.cs
@@ -2,28 +2,28 @@
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Bienvenido al Derivadas Arena"); Console.WriteLine(" "); Console.WriteLine("Está comprando entradas para el partido Inter Derivadas vs Integrales FC"); Console.WriteLine(" ");
         Console.WriteLine("                   *   *   *   *   *   *   *   *   *   *   *   *   *");
         Console.WriteLine("               *  *                                             *      *");
         Console.WriteLine("            *      *                                           *          *");
         Console.WriteLine("         *          *                    2                    *              *");
         Console.WriteLine("       *             *                                       *                 *");
         Console.WriteLine("     *                *                                     *                    *");
-        Console.WriteLine("   *                   *   *   *   *   *   *   *   *   *   *                       *");
+        Console.Write("   *"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("                   *   *   *   *   *   *   *   *   *   *"); Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("                       *");
         Console.WriteLine("  *                                                                                 *");
-        Console.WriteLine(" *                     *                                   *                         *");
+        Console.Write(" *"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("                     *                                   *"); Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("                          *");
         Console.WriteLine("*                                                                                     *");
-        Console.WriteLine("*                      *                                   *                          *");
+        Console.Write("*"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("                      *                                   *"); Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("                          *");
         Console.WriteLine("*        1                                                                3           *");
-        Console.WriteLine("*                      *                                   *                          *");
+        Console.Write("*"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("                      *                                   *"); Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("                          *");
         Console.WriteLine("*                                                                                     *");
-        Console.WriteLine(" *                     *                                   *                         *");
-        Console.WriteLine("  *                                                                                 *");
-        Console.WriteLine("   *                   *   *   *   *   *   *   *   *   *   *                       *");
+        Console.Write(" *"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("                     *                                   *"); Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("                         *");
+        Console.Write("  *"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("                    *   *   *   *   *   *   *   *   *   *"); Console.ForegroundColor = ConsoleColor.White; Console.WriteLine("                       *");
         Console.WriteLine("     *                *                                     *                    *");
         Console.WriteLine("       *             *                                       *                 *");
         Console.WriteLine("         *          *                    4                    *              *");
         Console.WriteLine("            *      *                                           *          *");
         Console.WriteLine("               *  *                                             *      *");
-        Console.WriteLine("                   *   *   *   *   *   *   *   *   *   *   *   *   *");
+        Console.WriteLine("                   *   *   *   *   *   *   *   *   *   *   *   *   *"); Console.WriteLine(" ");
     }
 }
